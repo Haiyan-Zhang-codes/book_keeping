f698f8871698236fee9ec78ff6ec1a05c097352f
Haiyan-Zhang1982 <haiyan.zhang1982@gmail.com>
2024-03-08 11:13:18 -0500

    finish register and login

---
+++ README.md
@@ -0,0 +1,3 @@
+# Book Keeping
+#### Video demo URL: 
+#### Description:
--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import os
+
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
+from cs50 import SQL
 
-import sqlite3
-conn = sqlite3.connect('data/app.db')
-cur = conn.cursor()
+db = SQL("sqlite:///data/app.db")
 
-from helpers.py import apology, login_required, lookup, usd
+from helpers import apology, login_required, lookup, usd
 
 app = Flask(__name__)
 
@@ -17,3 +18,58 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        name = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        if not name or not password or password != confirmation:
+           return apology("Field cannot be empty or password is not identical")
+        else:
+            hashedPassword = generate_password_hash(password)
+            try:
+                db.execute("INSERT INTO users (username, hash) VALUES(?,?)", name, hashedPassword)
+                return render_template("login.html")
+            except (ValueError):
+                return apology("username exists")
+            
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+        session["user_id"] = rows[0]["id"]
+        return redirect("/book_keeping")
+    else:
+        return render_template("login.html")
+
+
+@app.route("/book_keeping")
+def book_keeping():
+    return render_template("book_keeping.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
